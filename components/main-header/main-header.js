2aea69ad69e80dd3252ec371605db75d4f5d486d
MHM056 <mihailmarholev@gmail.com>
2025-03-03 21:55:43 +0200

    Updated main header component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,8 +4,10 @@ import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
+
     return (
         <>
             <MainHeaderBackground />
@@ -17,8 +19,8 @@ export default function MainHeader() {
 
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals">Browse Meals</Link></li>
-                        <li><Link href="/community">Foodies Community</Link></li>
+                        <li><NavLink href='/meals'>Browse Meals</NavLink></li>
+                        <li><NavLink href='/community'> Foodies Community</NavLink></li>
                     </ul>
                 </nav>
             </header>
