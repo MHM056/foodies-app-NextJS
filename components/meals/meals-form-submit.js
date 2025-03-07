8a205f5c13ed010e24cbe45d854e3b755d77d6bf
MHM056 <mihailmarholev@gmail.com>
2025-03-07 20:47:46 +0200

    Updated MealsFormSubmit Component

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -4,7 +4,7 @@ import { useFormStatus } from "react-dom";
 
 export default function MealsFormSubmit() {
     const { pending } = useFormStatus()
-    return <button disabled={pending}>;
+    return <button disabled={pending}>
         {pending ? 'Submitting...' : 'Share Meal'}
     </button>
 }
